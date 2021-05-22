0a06e82e1d9343814d70dc0dac25f701f59be700
NiverMtz <nivermartinezmed@gmail.com>
2021-05-21 20:13:57 -0500

    Parte 1 lista

---
+++ Archivos iniciales/1-Introducción/4-Comentarios/js/app.js
@@ -0,0 +1,7 @@
+//alert('Hola');
+
+console.log('Hola consola');
+
+var nombre = 'Marlen';
+
+console.log('El nombre es: ', nombre);
---
+++ Archivos iniciales/1-Introducción/5-StrictMode/js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+//alert('Hola');
+
+console.log('Hola consola');
+
+var nombre = 'Marlen';
+
+console.log('El nombre es: ', nombre);
+
+var publicData = "Hola";
